f97f255abdf78667273a1ced332856707b8340fb
kdkpras <101487383+kdkpras@users.noreply.github.com>
2022-11-03 20:36:24 +0800

    Add files via upload

---
+++ project_/JF_V02_S03_L01PrjStudent/RobotWorld.java
@@ -0,0 +1,19 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class RobotWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     */
+    public RobotWorld()
+    {
+        super(600, 400, 1);
+    }
+}
